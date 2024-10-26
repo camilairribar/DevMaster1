f65ba8b43b24bad7135d654e647c96f40f55fea2
camil <@algo>
2024-10-25 23:18:50 -0300

    Se agrega el Proyecto

---
+++ src/main/java/com/PoloDeSalud/UBB/UbbApplication.java
@@ -0,0 +1,13 @@
+package com.PoloDeSalud.UBB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UbbApplication.class, args);
+
+	}
+}
---
+++ src/test/java/com/PoloDeSalud/UBB/UbbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PoloDeSalud.UBB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UbbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
