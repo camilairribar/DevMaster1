e9ddac8124920a29d91bb3708ffaf46132e662f0
matias0114 <matias.jara1901@alumnos.ubiobio.cl>
2024-10-30 22:28:21 -0300

    actualizacion NoticiaService, NoticiaServiceImpl

--- src/main/java/com/PoloDeSalud/UBB/rest/NoticiaController.java
+++ src/main/java/com/PoloDeSalud/UBB/rest/NoticiaController.java
@@ -4,7 +4,6 @@ import com.PoloDeSalud.UBB.model.Noticia;
 import com.PoloDeSalud.UBB.service.NoticiaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
