1c640a660b8c6605a1659a5fe28ce220cba9fa37
JavierParra2101 <javier.parra2101@alumnos.ubiobio.cl>
2024-11-04 22:43:25 -0300

    Creación de ProyectoAutor

---
+++ src/main/java/com/PoloDeSalud/UBB/model/ProyectoAutor.java
@@ -0,0 +1,40 @@
+package com.PoloDeSalud.UBB.model;
+
+import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+@Table(name = "Proyecto_Autor")
+public class ProyectoAutor {
+    @Id
+    @Column(name = "id_autor")
+    private int idAutor;
+
+    @Id
+    @Column(name = "id_proyecto")
+    private int idProyecto;
+
+    // Getters and Setters
+    public int getIdAutor() {
+        return idAutor;
+    }
+
+    public void setIdAutor(int idAutor) {
+        this.idAutor = idAutor;
+    }
+
+    public int getIdProyecto() {
+        return idProyecto;
+    }
+
+    public void setIdProyecto(int idProyecto) {
+        this.idProyecto = idProyecto;
+    }
+}
