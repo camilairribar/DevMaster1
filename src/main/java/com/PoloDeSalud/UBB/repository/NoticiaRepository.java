fa3f20ecd416dcceb3c16859462cc90a72964230
matias0114 <matias.jara1901@alumnos.ubiobio.cl>
2024-11-28 17:33:27 -0300

    Frontend Noticia

--- src/main/java/com/PoloDeSalud/UBB/repository/NoticiaRepository.java
+++ src/main/java/com/PoloDeSalud/UBB/repository/NoticiaRepository.java
@@ -1,12 +1,12 @@
 package com.PoloDeSalud.UBB.repository;
 
-import com.PoloDeSalud.UBB.model.Noticia;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
-
 import java.util.Date;
 import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.PoloDeSalud.UBB.model.Noticia;
 
 public interface NoticiaRepository extends JpaRepository<Noticia, Integer> {
     // Buscar noticias por título
--- src/main/java/com/PoloDeSalud/UBB/rest/NoticiaController.java
+++ src/main/java/com/PoloDeSalud/UBB/rest/NoticiaController.java
@@ -1,27 +1,44 @@
 package com.PoloDeSalud.UBB.rest;
 
-import com.PoloDeSalud.UBB.model.Noticia;
-import com.PoloDeSalud.UBB.service.NoticiaService;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.text.SimpleDateFormat;
-import java.util.List;
-import java.util.Date;
+import com.PoloDeSalud.UBB.model.Noticia;
+import com.PoloDeSalud.UBB.service.NoticiaService;
 
+@CrossOrigin(origins = "http://127.0.0.1:3000") // Permite CORS solo para este controlador
 @RestController
 @RequestMapping("/noticias")
 public class NoticiaController {
 
     @Autowired
-    private NoticiaService noticiaService;
+    NoticiaService noticiaService;
 
-    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+    static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
-    @GetMapping
-    public List<Noticia> obtenerTodas() {
-        return noticiaService.obtenerTodas();
+    //@GetMapping
+    //public List<Noticia> obtenerTodas() {
+    //    return noticiaService.obtenerTodas();
+    //}
+    @GetMapping("/ListaNoticia")
+    public ResponseEntity<List<Noticia>> obtenerTodas() {
+        List<Noticia> lista= noticiaService.obtenerTodas();
+        return new ResponseEntity<List<Noticia>>(lista,HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
